90ec92b236860c0e9fe28a3c9d53a25400f07125
Arjun G Ravi <arjungravi1@gmail.com>
2023-05-10 20:04:26 +0530

    done hanging

--- Hangman.py
+++ Hangman.py
@@ -7,6 +7,7 @@ num_turns = 5
 
 alphabets = list(string.ascii_uppercase)
 alphabets_user = []
+correct_alph = []
 word = random.choice(words).upper()
 w = '-' * len(word)
 
@@ -25,10 +26,10 @@ def get_char(a):
         return a 
          
 i = 0
-print(word)
-
+print("\nLETS PLAY HANGMAN ...")
+print("TRY TO GUESS MY WORD")
+print(f"It is {len(word)} lettered word.\n\n")
 while True:
-    hit = 0
     if i == num_turns:
         print("You lost the game")
         print("The word was", word)
@@ -36,23 +37,24 @@ while True:
     print(f"The word is: {w}")
     inp = input("Enter an alphabet:")
     let = get_char(inp)
-    print(f'Letters used = {alphabets_user}')
-    i = len(alphabets_user)
+    # print(f'Letters used = {alphabets_user}')
+    # print(f'Correct Letters used = {correct_alph}')
+
+    
     if let is not None:
         for j in range(len(word)):
             if word[j] == let:
-                hit = 1
                 word_as_list = list(w)
                 word_as_list[j] = let
+                correct_alph.append(let)
+                correct_alph = list(set(correct_alph))
                 w = ''.join(word_as_list)
-      
+    i = len(alphabets_user) - len(correct_alph)  
     if word == w:
         print()
         print("You won the game!")
         print("The word indeed is", word)
         break
-
+    # print(i)
     print(f"Number of turns left = {num_turns - i }")
     print()
-
-
