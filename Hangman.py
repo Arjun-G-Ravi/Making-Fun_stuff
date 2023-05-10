457f0683c6b57dcfa4a5d5b2b69639d8b1bc19d1
Arjun G Ravi <arjungravi1@gmail.com>
2023-05-10 19:45:26 +0530

    hh

--- Hangman.py
+++ Hangman.py
@@ -25,19 +25,33 @@ def get_char(a):
         return a 
          
 i = 0
+print(word)
+
 while True:
+    hit = 0
     if i == num_turns:
         print("You lost the game")
+        print("The word was", word)
         break
+    print(f"The word is: {w}")
     inp = input("Enter an alphabet:")
     let = get_char(inp)
     print(f'Letters used = {alphabets_user}')
     i = len(alphabets_user)
+    if let is not None:
+        for j in range(len(word)):
+            if word[j] == let:
+                hit = 1
+                word_as_list = list(w)
+                word_as_list[j] = let
+                w = ''.join(word_as_list)
+      
     if word == w:
         print()
         print("You won the game!")
+        print("The word indeed is", word)
         break
-    
+
     print(f"Number of turns left = {num_turns - i }")
     print()
 
