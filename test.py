91d4a80bac98eea7775bb4b0c4bfc32f42fd0b59
Arjun G Ravi <arjungravi1@gmail.com>
2023-05-25 05:40:29 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+class TestClass:
+    def __init__(self, name):
+        self.chellappan = name
+    def get_name(self):
+        print(self.chellappan)
+
+t1 = TestClass("Tim")
+t1.get_name()
+t2 = TestClass("T0m")
+t2.get_name()
